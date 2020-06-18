f12e60e752e89d73d652f94946e41b1aa070afd6
pav1978 <pavelek.le@gmail.com>
2020-06-18 14:50:01 +0100

    creation of home controller and refacring of list.html template

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -1,5 +1,6 @@
 package com.vega.springit.controller;
 
+import com.vega.springit.repository.LinkRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LinkController {
 
-    @GetMapping("/foo")
-    public String foo(Model model){
-        model.addAttribute("pageTitle", "This age is FOO!");
-        return "foo";
+    private LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+
+    @GetMapping("/")
+    public String list(Model model){
+        model.addAttribute("links", linkRepository.findAll());
+        return "link/list";
 
     }
 }
