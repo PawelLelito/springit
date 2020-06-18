2b289ad6ccf1ebe835b49ff8f69c31e4745b3c19
pav1978 <pavelek.le@gmail.com>
2020-06-18 16:15:36 +0100

    set up of comment view

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -1,9 +1,13 @@
 package com.vega.springit.controller;
 
+import com.vega.springit.model.Link;
 import com.vega.springit.repository.LinkRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class LinkController {
@@ -14,11 +18,25 @@ public class LinkController {
         this.linkRepository = linkRepository;
     }
 
-
     @GetMapping("/")
     public String list(Model model){
         model.addAttribute("links", linkRepository.findAll());
         return "link/list";
+
+    }
+
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id,  Model model){
+        Optional<Link> link = linkRepository.findById(id);
+
+        if(link.isPresent()){
+            model.addAttribute("link",link.get());
+            return "link/view";
+
+        }else{
+
+            return "redirect:/";
+        }
 
     }
 }
