cfec40c377edd23d5f62eab9ed30d209fa72aef9
PawelLelito <pavelek.le@gmail.com>
2020-06-20 18:49:35 +0100

    creation of submit page along with validation methods

--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -2,17 +2,25 @@ package com.vega.springit.controller;
 
 import com.vega.springit.model.Link;
 import com.vega.springit.repository.LinkRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Optional;
+
 
 @Controller
 public class LinkController {
 
     private LinkRepository linkRepository;
+    private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
 
     public LinkController(LinkRepository linkRepository) {
         this.linkRepository = linkRepository;
@@ -31,6 +39,7 @@ public class LinkController {
 
         if(link.isPresent()){
             model.addAttribute("link",link.get());
+            model.addAttribute("success",model.containsAttribute("success"));
             return "link/view";
 
         }else{
@@ -38,5 +47,29 @@ public class LinkController {
             return "redirect:/";
         }
 
+    }
+
+
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link",new Link());
+        return "link/submit";
+    }
+
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if( bindingResult.hasErrors() ) {
+            logger.info("Validation errors were found while submitting a new link, try again.");
+            model.addAttribute("link",link);
+            return "link/submit";
+        } else {
+            // save our link
+            linkRepository.save(link);
+            logger.info("New Link was saved successfully.");
+            redirectAttributes
+                    .addAttribute("id", link.getId())
+                    .addFlashAttribute("success",true);
+            return "redirect:/link/{id}";
+        }
     }
 }
